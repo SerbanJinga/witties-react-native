8fbd5493c685d20414128541c62f5a3146c071eb
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2020-05-01 14:48:38 +0300

    start

---
+++ src/styles/theme.js
@@ -0,0 +1,20 @@
+const colors = {
+    blue: '#0984e3',
+    gray: '#f5f6fa',
+    white: '#fff',
+    black: '#000'
+};
+
+const sizes = {
+
+};
+
+const fonts = {
+
+};
+
+export{
+    colors, 
+    sizes, 
+    fonts
+}
