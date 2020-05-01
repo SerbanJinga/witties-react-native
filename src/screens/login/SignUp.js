2242b8f6afb985d72c400820cb33ecb596ba0439
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2020-05-01 16:18:01 +0300

    in order to work

--- src/screens/login/SignUp.js
+++ src/screens/login/SignUp.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
-
+import { View, Dimensions, StyleSheet } from 'react-native'
+import { Text, Divider, Button, Input } from 'react-native-elements'
+import * as theme from '../../styles/theme'
+const { width, height } = Dimensions.get('window')
 export default class SignUp extends Component {
     constructor(props){
         super(props)
@@ -15,7 +17,25 @@ export default class SignUp extends Component {
 
     render(){
         return(
-            <Text style={{marginTop: 80}}>Sign Up</Text>
-            )
+            <View style={styles.container}>
+                <Text h1>da</Text>
+                <Text h2>da</Text>
+                <Text h3>da</Text>
+                <Text h4>da</Text>
+                <Button type="outline" title="Buton"/>
+                <Input placeholder="Email"/>
+                <Divider/>
+            </View>
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: width,
+        height: height
+    }, 
+    button: {
+        color: theme.colors.blue
+    }
+})
