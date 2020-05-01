2e597744ec1f80507935b669b516aad427da9562
Burbietta <46001963+Burbietta@users.noreply.github.com>
2020-05-01 14:16:08 +0300

    asd

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+const sos = 3;
 export default class App extends Component {
+    
   render(){
     return (
       <View style={styles.container}>
