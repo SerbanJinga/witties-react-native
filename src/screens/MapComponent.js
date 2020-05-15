248170976b8455c301b3ee9ca247d628d4c4d823
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2020-05-15 16:11:37 +0300

    Notifications & map component

--- src/screens/MapComponent.js
+++ src/screens/MapComponent.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Text, Button, Input, Image } from 'react-native-elements'
+import { Text, Button, Input } from 'react-native-elements'
 import {
     StyleSheet,
     View,
@@ -12,6 +12,7 @@ import {
     ImageBackground,
     Dimensions,
     LayoutAnimation,
+    Image,
 
 
 } from 'react-native';
@@ -20,8 +21,18 @@ import { Marker } from 'react-native-maps';
 import Icon from 'react-native-vector-icons/Ionicons'
 import Icon2 from 'react-native-vector-icons/MaterialIcons'
 import * as Location from 'expo-location'
+import firebase from 'firebase'
+import { max } from "react-native-reanimated";
 const { width, height } = Dimensions.get('window')
-
+const Valori = {
+    "image": "https://firebasestorage.googleapis.com/v0/b/witties.appspot.com/o/photos%2F5owDnkBxJIVrwT3Se45vWIVvrHF3%2F1588608892966.jpg?alt=media&token=8593317b-d2eb-4f54-96b9-0f866861d2df",
+    "location": {
+        "lat": 45,
+        "lng": 60,
+    },
+    "text": "salut baieti merge bine",
+    "userId": "3tHCTgdV7cP8pklBEVytjshNo1A2",
+}
 export default class MapComponent extends React.Component {
     constructor(props) {
         super(props)
@@ -30,22 +41,47 @@ export default class MapComponent extends React.Component {
             nameOfUser: props.name,
             password: '',
             gap: 0,
+            sizeImage: 100,
+            region: {
+                latitude: 37.78825,
+                longitude: 40.4324,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            },
             taggedUsers: [],
             oneUserTag: '',
-            location: []
-
+            location: [],
+            currentuser: firebase.auth().currentUser.uid,
+            markerData: [{
+                "image": "https://firebasestorage.googleapis.com/v0/b/witties.appspot.com/o/photos%2F5owDnkBxJIVrwT3Se45vWIVvrHF3%2F1588608892966.jpg?alt=media&token=8593317b-d2eb-4f54-96b9-0f866861d2df",
+                "location": {
+                    "lat": 45,
+                    "lng": 60,
+                },
+                "text": "salut baieti bine",
+                "userId": "3tHCTgdV7cP8pklBEVytjshNo1A2",
+            }]
+            // {
+            //     "image": "https://s.evz.ro/imgserv/1200x0/smart/filters:contrast(5):quality(80):format(jpeg)/evz.ro/wp-content/uploads/2019/09/florinsalam.jpg",
+            //     "location": {
+            //         "lat": 20,
+            //         "lng": 30,
+            //     },
+            //     "text": "merge bine",
+            //     "userId": "3tHCTgdV7cP8pklBEVytjshNo1A2",
+            // }
         }
     }
-
     componentDidMount() {
         this.retrieveLocation()
+        this.retrieveData()
         console.log("De aici vine", this.state.displayName)
         console.log(this.state.location)
     }
 
-    retrieveLocation = async() => {
+    retrieveLocation = async () => {
         let { status } = await Location.requestPermissionsAsync()
-        if(status !== 'granted'){
+        if (status !== 'granted') {
             alert('n-ai dat accept')
         }
 
@@ -55,27 +91,90 @@ export default class MapComponent extends React.Component {
         })
     }
 
+
+    async retrieveData() {
+        let initialQuery = await firebase.firestore().collection("media").where("userId", "==", "3tHCTgdV7cP8pklBEVytjshNo1A2")
+        let documentSnapshots = await initialQuery.get()
+        let documentData = documentSnapshots.docs.map(doc => doc.data())
+        this.setState({ markerData: documentData })
+        console.log('-------------------------------------------------')
+        console.log(this.state.markerData)
+        console.log('-------------------------------------------------')
+        console.log(this.state.markerData[0].location)
+
+    }
+
+    changeMarkerSize() {
+        let zoom = Math.max(this.state.region.latitudeDelta, this.state.region.longitudeDelta)
+        console.log(zoom)
+        if (zoom >= 10) {
+            this.setState({ sizeImage: 25 })
+
+            // console.log('>1')
+            console.log(this.state.sizeImage)
+
+        } else if (zoom < 10 && zoom >= 5) {
+            this.setState({ sizeImage: 50 })
+            // console.log('0.5-1')
+            console.log(this.state.sizeImage)
+
+        } else if (zoom < 5 && zoom >= 2.5) {
+            //  console.log('0.25-0.5')
+            this.setState({ sizeImage: 75 })
+            console.log(this.state.sizeImage)
+
+        } else if (zoom < 2.5) {
+            this.setState({ sizeImage: 100 })
+            // console.log('<0.25')
+            console.log(this.state.sizeImage)
+        }
+    }
+
+
     render() {
         let text = ''
-        text = JSON.stringify(this.state.location.latitude)
-        let text2 = JSON.stringify(this.state.location.longitude)
-return (<View style={styles.container}>
+        text = JSON.stringify(this.state.location)
+        return (<View style={styles.container}>
+            <Button style={{ marginTop: 40 }} onPress={() => { console.log(this.state.region) }} title='salll' />
+            <Button style={{ marginTop: 40 }} onPress={() => { console.log(this.state.region) }} title='salll2' />
+            <Button style={{ marginTop: 40 }} onPress={() => { this.changeMarkerSize() }} title='salll3' />
             <Text style={{ marginTop: 50 }}>{text}</Text>
-            <MapView style={styles.mapStyle} mapType='hybrid' initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0,
-                longitudeDelta: 0.0,
-            }}
+            <MapView style={styles.mapStyle} mapType='hybrid'
+                region={this.state.region}
+                // initialRegion={{
+                //     latitude: 45,
+                //     longitude: 60,
+                //     latitudeDelta: 50,
+                //     longitudeDelta: 0.0,
+                // }}
+                onRegionChangeComplete={(region) => {
+                    this.setState({ region: region })
+                    this.changeMarkerSize()
+                }}
             >
 
-                <Marker
-                    coordinate={{ latitude: text, longitude: text2}}
+                {this.state.markerData.map(marker => (
+                    <View style={{}}>
+                        <Marker
+                            coordinate={{ latitude: marker.location.lat, longitude: marker.location.lng }}
+                            title={marker.text}>
+                        <View style={{height:this.state.sizeImage *2,width:this.state.sizeImage *2,  }}>
+                            <Image
+                                source={{ uri: marker.image }}
+                                
+                                style={{
+                                    flex: 1,
+                                    alignSelf: 'stretch',
+                                    width: undefined,
+                                    height: undefined,
+                                    resizeMode:'cover'
+                                }}
 
-                    title={'Report Team'}
-                    description={"aici se afla cel mai toxic kid"}
-                />
-               7]
+                            />
+                        </View>
+                        </Marker>
+                    </View>
+                ))}
 
             </MapView>
         </View>)
@@ -83,7 +182,6 @@ return (<View style={styles.container}>
 
 
 }
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -93,5 +191,14 @@ const styles = StyleSheet.create({
     mapStyle: {
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height,
+    },
+    box: {
+        height: 100,
+        width: 100,
+        borderRadius: 5,
+        marginVertical: 40,
+        backgroundColor: "#61dafb",
+        alignItems: "center",
+        justifyContent: "center"
     },
 })
